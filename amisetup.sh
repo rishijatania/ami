29a2754a2b22bf3083b0b527908ea21890889cb6
rishijatania <jatania.r@husky.neu.edu>
2020-03-07 20:28:02 -0500

    Ami install codedeploy agent -15

--- amisetup.sh
+++ amisetup.sh
@@ -5,8 +5,7 @@ sudo apt-get install nodejs -y
 
 sudo apt-get install ruby -y
 sudo apt-get install wget -y
-echo ${CODEDEPLOY_AGENT_URL}
-wget "${CODEDEPLOY_AGENT_URL}"
+wget https://"${BUCKET_NAME}".s3."${AWS_REGION}".amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
