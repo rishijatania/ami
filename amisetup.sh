af76a1e86fbbdfac3d0a2a54f6b9e8054fc034d3
rishijatania <jatania.r@husky.neu.edu>
2020-03-10 11:44:20 -0400

    Ami unstall unzip

--- amisetup.sh
+++ amisetup.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install nodejs -y
+sudo apt-get install unzip -y
 
 sudo apt-get install ruby -y
 sudo apt-get install wget -y
