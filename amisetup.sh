a7d60af6198fd75fb6d4745025c3ff4247f91726
rishijatania <jatania.r@husky.neu.edu>
2020-03-16 21:23:11 -0400

    Adding status check for cloud watch

--- amisetup.sh
+++ amisetup.sh
@@ -15,3 +15,4 @@ sudo service codedeploy-agent status
 
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
